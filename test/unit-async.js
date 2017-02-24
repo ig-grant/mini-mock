8e2b508c78b0f62a4603a111c9508381764aae9c
Grant <grantpidwell@gmail.com>
2017-02-24 09:57:59 +0000

    Reformatted

--- test/unit-async.js
+++ test/unit-async.js
@@ -8,13 +8,13 @@ var Motor = require('./lib/motor');
 var MotorFactory = require('./lib/motor-factory');
 var Mocker = require('../lib/mocker');
 
-describe('unit - robot', function () {
+describe('unit - robot', function() {
 
-    beforeEach('setup', function (done) {
+    beforeEach('setup', function(done) {
         var mocker = new Mocker();
 
         this.__motor = mocker.mock(Motor.prototype)
-            .withStubAsyncFunc('start', [null, 250])// callback args are [null, 250]
+            .withStubAsyncFunc('start', [null, 250]) // callback args are [null, 250]
             .create();
 
         this.__motorFactory = mocker.mock(MotorFactory.prototype)
@@ -24,12 +24,12 @@ describe('unit - robot', function () {
         done();
     });
 
-    it('successfully uses async mock', function (done) {
+    it('successfully uses async mock', function(done) {
 
         var self = this;
 
         var robot = new Robot(this.__motorFactory);
-        robot.walk(function (e, result) {
+        robot.walk(function(e, result) {
             expect(self.__motorFactory.recorder['getMotor'].calls).to.equal(1);
             expect(self.__motor.recorder['start'].calls).to.equal(1);
             expect(result).to.equal(250);
